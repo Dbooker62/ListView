74e1e17a3d072fb5c9d2f2f4dac78c152b5fc56c
Dbooker62 <dbooker29@students.mchenry.edu>
2023-11-27 23:36:52 -0600

    commit

---
+++ app/src/main/java/com/example/listview/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.listview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val listView = findViewById<ListView>(R.id.listview)
+        val name = arrayOf("Android" ,"PHP" , "Java" , "C++" , "ASP .NET" , "Swift" )
+        val arrayAdapter: ArrayAdapter<String> = ArrayAdapter(
+            this, android.R.layout.simple_list_item_1, name
+        )
+        listView.adapter = arrayAdapter
+        listView.setOnItemClickListener { adapterView, view, i, l ->
+            Toast.makeText(this , "Item Selcted" + name[i] ,Toast.LENGTH_LONG ).show()
+        }
+    }
+}
+
+
